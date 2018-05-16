85f86133b0e6b881982d72f54d6a4edb3b381ac1
Corey Sizemore <coreysizemore@gmail.com>
2018-05-15 17:10:12 -0700

    v1.0.0
    Moved to Galaxy Theme.

--- thegalaxy-child/lib/wordpress_child.php
+++ thegalaxy-child/lib/wordpress_child.php
@@ -7,8 +7,6 @@
 	function csdd_child_scripts() {
 	
 		wp_enqueue_style( 'csdd-custom', get_stylesheet_directory_uri() . '/css/custom.css' );
-		
-		wp_enqueue_style( 'csdd-colors', get_stylesheet_directory_uri() . '/css/colors.css' );
 	
 	}
 	add_action( 'wp_enqueue_scripts', 'csdd_child_scripts' );
